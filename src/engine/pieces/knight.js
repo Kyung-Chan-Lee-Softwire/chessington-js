5453c1d5a14f685585b9596ec48cb0ac772afe30
Kyung Lee <kyung.lee@softwire.com>
2019-07-01 14:49:03 +0100

    Test for Knight capture complete

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -12,14 +12,22 @@ export default class Knight extends Piece {
         const currentSquare = board.findPiece(this);
         const row = currentSquare.row;
         const col = currentSquare.col;
-        if(this.validCoordinate(row + 2,col + 1)) moves.push(Square.at(row + 2,col + 1));
-        if(this.validCoordinate(row - 2,col + 1)) moves.push(Square.at(row - 2,col + 1));
-        if(this.validCoordinate(row + 2,col - 1)) moves.push(Square.at(row + 2,col - 1));
-        if(this.validCoordinate(row - 2,col - 1)) moves.push(Square.at(row - 2,col - 1));
-        if(this.validCoordinate(row + 1,col + 2)) moves.push(Square.at(row + 1,col + 2));
-        if(this.validCoordinate(row - 1,col + 2)) moves.push(Square.at(row - 1,col + 2));
-        if(this.validCoordinate(row + 1,col - 2)) moves.push(Square.at(row + 1,col - 2));
-        if(this.validCoordinate(row - 1,col - 2)) moves.push(Square.at(row - 1,col - 2));
+        let pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row + 2, col + 1));
+        if(this.validCoordinate(row + 2,col + 1) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row + 2,col + 1));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row - 2, col + 1));
+        if(this.validCoordinate(row - 2,col + 1) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row - 2,col + 1));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row + 2, col - 1));
+        if(this.validCoordinate(row + 2,col - 1) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row + 2,col - 1));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row - 2, col - 1));
+        if(this.validCoordinate(row - 2,col - 1) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row - 2,col - 1));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row + 1, col + 2));
+        if(this.validCoordinate(row + 1,col + 2) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row + 1,col + 2));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row - 1, col + 2));
+        if(this.validCoordinate(row - 1,col + 2) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row - 1,col + 2));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row + 1, col - 2));
+        if(this.validCoordinate(row + 1,col - 2) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row + 1,col - 2));
+        pieceOnSquare = !this.validCoordinate(row - 2,col - 1) || board.getPiece(Square.at(row - 1, col - 2));
+        if(this.validCoordinate(row - 1,col - 2) && (!pieceOnSquare || (pieceOnSquare.player != this.player && !(pieceOnSquare instanceof King)))) moves.push(Square.at(row - 1,col - 2));
         return moves;
     }
 }
