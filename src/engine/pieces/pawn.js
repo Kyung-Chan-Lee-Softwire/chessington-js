2d66425edd17159ca42fa95396396759845ad4cb
Kyung Lee <kyung.lee@softwire.com>
2019-07-01 11:29:22 +0100

    Test Red-2 passing

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Square from '../square';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -7,9 +8,19 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        let moves = new Array(2);
-        moves[0] = Square.at(1,0);
-        moves[1] = Square.at(6,7);
+        let moves = new Array(0);
+        if(this.player == Player.WHITE){
+            moves.push(Square.at(board.findPiece(this).row + 1,board.findPiece(this).col));
+            if(!this.hasMoved){
+                moves.push(Square.at(board.findPiece(this).row + 2,board.findPiece(this).col));
+            }
+        }
+        if(this.player == Player.BLACK){
+            moves.push(Square.at(board.findPiece(this).row - 1,board.findPiece(this).col));
+            if(!this.hasMoved){
+                moves.push(Square.at(board.findPiece(this).row - 2,board.findPiece(this).col));
+            }
+        }
         return moves;
     }
 }
