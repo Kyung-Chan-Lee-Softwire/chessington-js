909ed9a17045230002511dddad4c00df2a45fcfe
Kyung Lee <kyung.lee@softwire.com>
2019-07-01 12:01:46 +0100

    Test Red-7 Passing

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,17 +9,29 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let moves = new Array(0);
-        if(this.player == Player.WHITE){
+        while (this.player == Player.WHITE){
+            if(board.getPiece(Square.at(board.findPiece(this).row + 1,board.findPiece(this).col))) break;
+
             moves.push(Square.at(board.findPiece(this).row + 1,board.findPiece(this).col));
-            if(!this.hasMoved){
+
+            if(board.getPiece(Square.at(board.findPiece(this).row + 2,board.findPiece(this).col))) break;
+
+            if(!this.hasMoved) {
                 moves.push(Square.at(board.findPiece(this).row + 2,board.findPiece(this).col));
             }
+            break;
         }
-        if(this.player == Player.BLACK){
+        while (this.player == Player.BLACK){
+            if(board.getPiece(Square.at(board.findPiece(this).row - 1,board.findPiece(this).col))) break;
+
             moves.push(Square.at(board.findPiece(this).row - 1,board.findPiece(this).col));
+
+            if(board.getPiece(Square.at(board.findPiece(this).row - 2,board.findPiece(this).col))) break;
+
             if(!this.hasMoved){
                 moves.push(Square.at(board.findPiece(this).row - 2,board.findPiece(this).col));
             }
+            break;
         }
         return moves;
     }
